478f94eeb9018432b225b58bca4a014561d605a0
sohailarman <sohailarman@gmail.com>
2022-09-01 19:54:24 +0600

    Mi store clone project started

---
+++ src/components/PreNavbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/PreNavbar.css'
+
+const PreNavbar = () => {
+  return (
+    <div className='preNav'>
+        <div>
+            <a href="https://www.mi.com/us">XIAOMI UNITED STATES</a> <span> | </span>
+            <a href="https://en.miui.com">MIUI</a> <span> | </span>
+            <a href="https://www.facebook.com/xiaomiusa">FIND US ON FACEBOOK</a>
+        </div>
+
+    </div>
+  )
+} 
+
+export default PreNavbar
