6d8d4259240e9072b03d008520c496f1fe751cae
sohailarman <sohailarman@gmail.com>
2022-10-15 13:33:46 +0600

    site complete

--- src/components/App.js
+++ src/components/App.js
@@ -19,15 +19,56 @@ import data from "../data/data.json"
 
 
 
-
-
-
-
 function App() {
 
   const [toggleClass, setToggleClass] = useState(false);
   const [navMenuPop, setNavMenuPop] = useState(false);
 
+  function getImageBrightness(image) {
+
+    var img = document.createElement("img");
+    img.src = image;
+    img.crossOrigin = 'anonymous';
+    img.style.display = "none";
+    document.body.appendChild(img);
+  
+    var colorSum = 0;
+  
+    img.onload = function() {
+        var canvas = document.createElement("canvas");
+        canvas.width = this.width;
+        canvas.height = this.height;
+  
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this,0,0);
+  
+        var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+        var data = imageData.data;
+        var r,g,b,avg;
+  
+          for(var x = 0, len = data.length; x < len; x+=4) {
+            r = data[x];
+            g = data[x+1];
+            b = data[x+2];
+  
+            avg = Math.floor((r+g+b)/3);
+            colorSum += avg;
+        }
+  
+        var brightness = Math.floor(colorSum / (this.width*this.height));
+        // console.log(brightness);
+        // console.log(image);
+  
+        if (brightness > 95) {
+          setToggleClass(true)
+  
+        }
+        else {
+          setToggleClass(false)
+        }
+  
+    }
+  }
 
   return (
   <Router>
@@ -49,7 +90,7 @@ function App() {
 
     
 
-    <Slider toggleClass={toggleClass} setToggleClass={setToggleClass} startImage={data.banner.start} />
+    <Slider getImageBrightness={getImageBrightness} toggleClass={toggleClass} startImage={data.banner.start} />
 
     <Offers offer={data.offer} />
 
